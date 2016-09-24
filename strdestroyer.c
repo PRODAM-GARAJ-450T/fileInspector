1c07b0713e0694e004ffd1573170801de1374d89
PRODAM-GARAJ-450T <saryman1972@gmail.com>
2016-09-24 13:41:39 +0400

    stabel

--- strdestroyer.c
+++ strdestroyer.c
@@ -7,10 +7,11 @@
 //  int wordsCount;
 //} strDitales;
 
-void strFromStr(char *str, char *word)
+int strFromStr(char *str, char *word)
 {
   int length1 = 0,
-      length2 = 0;
+      length2 = 0,
+      size = 0;
   char *subBuffer = {0};
 
   length1 = strlen(str);
@@ -21,8 +22,11 @@ void strFromStr(char *str, char *word)
   else
     length2 = strlen(subBuffer);
 
-  strncpy(word, str, length1 - length2 );
+  size = length1 - length2;
+
+  strncpy(word, str, size );
 //  str = strstr( str, "/");
+  return size;
 }
 
 void destroyertowords(char *str, strDitales *strD)
@@ -31,9 +35,7 @@ void destroyertowords(char *str, strDitales *strD)
   strcpy(strD->full, str);
 
   char word[32] = {0};
-  int i = 0,
-      length1 = 0,
-      length2 = 0,
+  int size = 0,
       wordsCount = 0;
 
      char *p = str;
@@ -43,36 +45,36 @@ void destroyertowords(char *str, strDitales *strD)
        ++p;
        wordsCount++;
        strD->words = (char**) realloc(strD->words, (wordsCount + 1) * sizeof(char*));
-       strFromStr( &str, &word);
+       size = strFromStr( str, &word);
 
        while ( str != p )
        {
          *str++;
        }
 
-       strD->words[wordsCount] = (char*) malloc( length1 - length2 + 1 );
+       strD->words[wordsCount] = (char*) malloc( size + 1 );
 
        strcpy(strD->words[wordsCount], word);
 
-// todo del      printf("%s", strD->words[wordsCount]);
+/*// todo del */     printf("%s\n", strD->words[wordsCount]);
 
 // todo del      putchar('\n');
      }
 
-     strFromStr( str, &word);
+     size = strFromStr( str, &word);
 
      while ( *str )
      {
        *str++;
      }
 
-     strD->words[wordsCount] = (char*) malloc( length1 - length2 + 1 );
+     strD->words[wordsCount] = (char*) malloc( size + 1 );
 
      strcpy(strD->words[wordsCount], word);
-     wordsCount++;
+//     wordsCount++;
 
      strD->wordsCount = wordsCount;
-//  todo del   printf("%s", strD->words[wordsCount]);
+/*//  todo del*/  printf("%s\n", strD->words[wordsCount]);
 
 //     for (int i = 0; i < wordsCount; ++i)
 //       {
